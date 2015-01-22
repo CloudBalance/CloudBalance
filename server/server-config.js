45f968dd5fcbe3768dfdd72f5298208358c65d7e
mc <mc@mcs-MacBook-Pro.local>
2015-01-22 15:16:26 -0800

    Adds basic server

--- server/server-config.js
+++ server/server-config.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var partials = require('express-partials');
 var util = require('./lib/utility');
+var fs = require('fs');
+var https = require('https');
 
 var handler = require('./lib/request-handler');
 
 var app = express();
+var router = express.Router();
 
-var router = express.router();
+// paths
+  //
+
+app.use()
+
+
+
