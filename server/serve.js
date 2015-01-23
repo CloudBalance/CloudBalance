51dcee2d73dba6b22916413b102f5435217ad489
mc <mc@mcs-MacBook-Pro.local>
2015-01-22 21:21:57 -0800

    works on dropbox authentication process, but not likely to use

--- server/serve.js
+++ server/serve.js
@@ -7,20 +7,14 @@ var app = express();
 
 app.use( express.static(__dirname + '/') );
 
-// var options = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// };
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
-// https.createServer(options, app).listen(8000);
+https.createServer(options, app).listen(8000);
 
 
 
-// FIXME: this should go in another file, but here for testing
-app.use('/auth/google/callback', function(req, res, next) {
-  console.log(res);
-  res.redirect('/');
-});
-
-app.listen(8000);
+// app.listen(8000);
 console.log('listening on port 8000');
