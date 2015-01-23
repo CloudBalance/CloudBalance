3261abaed480b29bd647f9faa9a9163e1aae0f77
mc <mc@mcs-MacBook-Pro.local>
2015-01-22 19:54:31 -0800

    gets basic server serve.js working, added a test to it for google api testing; removes files with leading underscores

--- server/serve.js
+++ server/serve.js
@@ -7,6 +7,7 @@ var app = express();
 
 app.use( express.static(__dirname + '/') );
 
+<<<<<<< HEAD
 var options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
@@ -17,4 +18,22 @@ https.createServer(options, app).listen(8000);
 
 
 // app.listen(8000);
+=======
+// var options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// };
+
+// https.createServer(options, app).listen(8000);
+
+
+
+// FIXME: this should go in another file, but here for testing
+app.use('/auth/google/callback', function(req, res, next) {
+  console.log(res);
+  res.redirect('/');
+});
+
+app.listen(8000);
+>>>>>>> gets basic server serve.js working, added a test to it for google api testing; removes files with leading underscores
 console.log('listening on port 8000');
