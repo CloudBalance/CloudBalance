e1d23e04dc82d4ffc941276f5a8cded4c6774466
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-01-24 16:07:20 -0800

    getAllFiles client-server api endpoint

--- server/api/api-router.js
+++ server/api/api-router.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var apiRouter = express.Router();
 
-apiRouter.use('/1/getAllFiles', function(req,res) {
-
-  res.end('working');
+apiRouter.get('/1/getAllFiles', function(req,res) {
+  //From Client: nothing
+  //To Client: two file lists to (one for each service)
+    //These file lists will be formatted in the exact same way so the client can just use the same function to render them, no matter which service the file list is coming from
+    //Format: roughly the same as here: 
+    //http://stackoverflow.com/questions/11194287/convert-a-directory-structure-in-the-filesystem-to-json-with-node-js
+    //keys: 
+      //fileID
+      //filename
+      //children: an array that holds objects that are either folders or files themselves. this obviously gets recursive
+      //fileIcon
+      //fileLink (though this won't be used by the client for MVP)
 });
 
 module.exports = apiRouter;
