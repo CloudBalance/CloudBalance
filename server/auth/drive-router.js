0da9bebc8f8cf63c2d34e93302db3afd8d839f09
Christian Reyes <csreyes@berkeley.edu>
2015-01-24 17:24:23 -0800

    working on server

--- server/auth/drive-router.js
+++ server/auth/drive-router.js
@@ -17,16 +17,46 @@ var scopes = [
 var callback = 'https://localhost:8000/auth/drive/callback';
 
 var url = oauth2Client.generateAuthUrl({
-  // access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-  scope: scopes // If you only need one scope you can pass it as string
+  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+  approval_prompt: 'force',
+  scope: scopes 
 });
 
-// oauth2Client.getToken(code, function(err, tokens) {
-//   // Now tokens contains an access_token and an optional refresh_token. Save them.
-//   if(!err) {
-//     oauth2Client.setCredentials(tokens);
-//   }
-// });
+// var printAbout = function() {
+//   var request = drive.about.get();
+//   request.execute(function(resp) {
+//     console.log('Current user name: ' + resp.name);
+//     console.log('Root folder ID: ' + resp.rootFolderId);
+//     console.log('Total quota (bytes): ' + resp.quotaBytesTotal);
+//     console.log('Used quota (bytes): ' + resp.quotaBytesUsed);
+//   });
+// }
+
+var getFiles = function(req, res, params) {
+
+	var list = [];
+	params = params || {};
+	drive.about.get({}, function(error, result) {
+		console.log(result)
+	})
+	// drive.files.list(params, function(error, result) {
+	// 	// var items = result.items;
+	// 	// for (var i = 0; i < items.length; i++) {
+	// 	// 	var item = items[i];
+	// 	// 	var listItem = {
+	// 	// 		title: item.title,
+	// 	// 		id: item.id,
+	// 	// 		alternateLink: item.alternateLink,
+	// 	// 		iconLink: item.iconLink,
+	// 	// 		createdDate: item.createdDate
+	// 	// 	};
+	// 	// 	list.push(listItem);
+	// 	// }
+	// 	// console.log(list);
+	// 	console.log(result);
+	// })
+	res.end();
+}
 
 var driveRouter = express.Router();
 
@@ -36,24 +66,19 @@ driveRouter.get('/', function(req, res) {
 
 driveRouter.get('/callback', function(req, res){
 	var code = req.query.code
-	oauth2Client.getToken(code, function(err, tokens) {
-		console.log('this is the error ', err);
-		console.log('this is the token ', tokens);
+	oauth2Client.getToken(code, function(error, tokens) {
+		if (error) {res.send(error)};
+		var accessToken = tokens.access_token;
+		var refresh_token = tokens.refresh_token;
 	  // Now tokens contains an access_token and an optional refresh_token. Save them.
 
 	  oauth2Client.setCredentials({
-	    access_token: tokens.access_token
-	    // refresh_token: 'REFRESH TOKEN HERE'
+	    access_token: tokens.access_token,
+	    refresh_token: tokens.refresh_token
 	  });
 
-	  drive.files.list({}, function(a,b) {console.log(b)});
-
-
-
-
-	  if(!err) {
-	    oauth2Client.setCredentials(tokens);
-	  }
+	  // drive.files.list({}, function(a,b) {console.log(b)});
+	  getFiles(req, res);
 	});
 	res.end();
 })
