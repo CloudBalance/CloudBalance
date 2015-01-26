60155ff5f23eb8d1b9fd1b6a1e8ed0ac9750bc19
mc <mc@mcs-macbook-pro.gateway.2wire.net>
2015-01-26 15:39:52 -0800

    adds initial directories and files for flux and react front-end

---
+++ public/js/actions/appActions.js
@@ -0,0 +1,26 @@
+// app-actions.js
+
+var AppConstants = require('../constants/appConstants.js');
+var AppDispatcher = require('../dispatcher/appDispatcher.js')
+
+var AppActions = {
+  // HAVE THESE ACTIONS ALIGN WITH CLIENT-SERVER API
+  addItem: function(item) {
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.ADD_ITEM,
+      item: item
+    })
+  },
+
+  increaseItem:function(index){
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.INCREASE_ITEM,
+      index: index
+    })
+  }
+
+
+};
+
+
+module.exports = AppActions;
---
+++ public/js/dispatcher/dispatcher.js
@@ -0,0 +1,51 @@
+// The GENERAL Dispatcher (boilerplate)
+
+// This file contains the two basic dispatcher methods: register and dispatch
+
+var Promise = require('es6-promise').Promise;
+var assign = require('object-assign');
+
+var _callbacks = [];
+var _promises = [];
+
+// Create Dispatcher class (pseudoclassical)
+var Dispatcher = function() {};
+Dispatcher.prototype = assign({}, Dispatcher.prototype, {
+
+  /** Register a Store's callback so that it may be invoked by an action.
+   * @param {function} callback The callback to be registered.
+   * @return {number} The index of the callback within the _callbacks array.  */
+  register: function(callback) {
+    _callbacks.push(callback);
+    return _callbacks.length - 1; // index
+  },
+
+  /** dispatch - triggers the invocation of the callbacks
+   * @param  {object} payload the data from the action.  */
+  dispatch: function(payload) {
+    // First create array of promises for callbacks to reference.
+    var resolves = [];
+    var rejects = [];
+    _promises = _callbacks.map(function(_, i) {
+      return new Promise(function(resolve, reject) {
+        // Track whether each newly promisified callback
+        // is resolved or rejected
+        resolves[i] = resolve;
+        rejects[i] = reject;
+      });
+    });
+    // Dispatch to callbacks and resolve/reject promises.
+    _callbacks.forEach(function(callback, i) {
+      // Callback can return an obj, to resolve, or a promise, to chain.
+      // See waitFor() for why this might be useful.
+      Promise.resolve(callback(payload)).then(function() {
+        resolves[i](payload);
+      }, function() {
+        rejects[i](new Error('Dispatcher callback unsuccessful'));
+      });
+    });
+    _promises = [];
+  }
+});
+
+module.exports = Dispatcher;
