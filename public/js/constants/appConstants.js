075a0f1a447ecb5f960cc33156c7ba92bc91a6b9
mc <mc@mcs-macbook-pro.gateway.2wire.net>
2015-01-27 11:01:30 -0800

    adds public react/flux files again, trying to complete rebase

--- public/js/constants/appConstants.js
+++ public/js/constants/appConstants.js
@@ -3,6 +3,7 @@ var keyMirror = require('keymirror');
 module.exports = keyMirror({
   // name all of our actions here:
 
+<<<<<<< HEAD
   UPDATE_FILE_LISTS: null,
   // CLOUDBALANCE_RENAME: null,
   // CLOUDBALANCE_DESTROY: null,
@@ -20,5 +21,22 @@ module.exports = keyMirror({
   // CLOUDBALANCE_MOVE_FROM_DRIVE_TO_DROPBOX: null,
   // CLOUDBALANCE_MOVE_WITHIN_DROPBOX: null,
   // CLOUDBALANCE_MOVE_WITHIN_DRIVE: null
+=======
+  ADD_ITEM: null,
+  INCREASE_ITEM: null,
+
+  CLOUDBALANCE_RENAME: null,
+  // CLOUDBALANCE_DESTROY: null,
+
+  CLOUDBALANCE_MOVE_FROM_DROPBOX: null,
+  CLOUDBALANCE_MOVE_FROM_DRIVE: null,
+  CLOUDBALANCE_MOVE_TO_DROPBOX: null,
+  CLOUDBALANCE_MOVE_TO_DRIVE: null,
+
+  CLOUDBALANCE_MOVE_FROM_DROPBOX_TO_DRIVE: null,
+  CLOUDBALANCE_MOVE_FROM_DRIVE_TO_DROPBOX: null,
+  CLOUDBALANCE_MOVE_WITHIN_DROPBOX: null,
+  CLOUDBALANCE_MOVE_WITHIN_DRIVE: null
+>>>>>>> adds public react/flux files again, trying to complete rebase
 
 });
