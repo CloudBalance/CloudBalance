27c5d147f3523ecbbd38e73f47db59ea190d5e46
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-01-30 13:58:32 -0800

    added comments for future todos while picking up the codebase

--- public/js/about.react.js
+++ public/js/about.react.js
@@ -3,6 +3,8 @@ var ABOUT = require('./components/about.react');
 var React = require('react');
 window.React = React; // export for http://fb.me/react-devtools  -  maybe not needed
 
+//we don't have a createComponent in here. 
+
 React.render(
   <ABOUT />,
   document.getElementById('about')
