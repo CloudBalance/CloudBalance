221bf2c330f6d0db13592e37648223c2650e6c54
Alan Sun <asun0927@gmail.com>
2015-02-02 10:31:35 -0800

    Move login authorization logic from app-config.js to the login router

--- server/account/account-router.js
+++ server/account/account-router.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var loginRouter = require('./login-router.js');
-var signupRouter = require('./signup-router.js');
+// var signupRouter = require('./signup-router.js');
 
 var accountRouter = express.Router();
 
-accountRouterRouter.use('/login', loginRouter);
-accountRouterRouter.use('/signup', signupRouter);
+accountRouter.use('/login', loginRouter);
+
+// accountRouterRouter.use('/signup', signupRouter);
 
 
-accountRouter.get('/logout', function(req, res) {
+// accountRouter.get('/logout', function(req, res) {
   //log the user out of both services
   //make sure we aren't storing the tokens anywhere
-})
+// })
 
 module.exports = accountRouter;
--- server/account/login-router.js
+++ server/account/login-router.js
@@ -1,9 +1,6 @@
 // var secrets = require('./secrets/dropbox.secret');
-var https = require('https');
 var express = require('express');
-var Promise = require('bluebird');
 
-var userAuthCode;
 var loginRouter = express.Router();
 
 // var getToken = function getToken(authCode) {
@@ -12,13 +9,9 @@ var loginRouter = express.Router();
 //   });
 // };
 
-loginRouter.get('/loginChain', function (req,res) {
-  //on the homepage, we will have a nice description of all the cool stuff our app does, to get the user interested
-  //if they're interested, we have a button called 'start' that will direct the user to auth/loginChain
-  //from here, we start the process of redirecting them to both cloud storage services where they will log in to each service
-  //while we could redirect directly to the first service from that 'start' button, using /loginChain keeps the code more modular and flexible for future changes
-
-  //ACTION: redirect to first cloud storage login
+loginRouter.get('/', function (req,res) {
+  //start off the login by redirecting to the google auth
+  res.redirect('/auth/drive');
 });
 
 module.exports = loginRouter;
