66b374f1fe5a9776db3c84edba336bc98d08cd79
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-02-01 19:10:11 -0800

    started mapping. last change (mostly working) before rebasing in server updates

--- public/js/components/Google.js
+++ public/js/components/Google.js
@@ -15,15 +15,42 @@ var Google = React.createClass({
         // This should go inside the ul, instead of just <File />
           // {fileListItems}
   render:function(){
+    console.log('this.props within Google.js');
+    console.log(this.props);
+    var fileListToDisplay = 
+      this.props.displayedGoogleFileList.map(function(file) {
+        return (
+          <li>
+            <File data={file} />
+          </li>
+        );
+      });
     return (
       <div className="files-container" id="google-container">
         <h3 className='service-title'>Google Drive</h3>
         <ul className="file-list">
+          {fileListToDisplay}
         </ul>
       </div>
     );
   },
 
 });
+
+//example pattern from tutorial: 
+// render: function() {
+//   var commentNodes = this.props.data.map(function (comment) {
+//     return (
+//       <Comment author={comment.author}>
+//         {comment.text}
+//       </Comment>
+//     );
+//   });
+//   return (
+//     <div className="commentList">
+//       {commentNodes}
+//     </div>
+//   );
+// }
 
 module.exports = Google;
